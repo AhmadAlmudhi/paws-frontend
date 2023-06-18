b46db6ceaf31c9d7f09be5e19e2c616fd56c6092
AhmadAlmudhi <a.almudhi1419@gmail.com>
2023-06-18 13:20:26 +0300

    Edit: nav bar

---
+++ paws_frontend/lib/screens/Nav_screens/offers_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OffersScreen extends StatelessWidget {
+  const OffersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        backgroundColor: Colors.grey,
+        body: Center(
+          child: Text("NAV1"),
+        ));
+  }
+}
---
+++ paws_frontend/lib/screens/Nav_screens/requests_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RequestsScreen extends StatelessWidget {
+  const RequestsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        backgroundColor: Colors.grey,
+        body: Center(
+          child: Text("nav2"),
+        ));
+  }
+}
