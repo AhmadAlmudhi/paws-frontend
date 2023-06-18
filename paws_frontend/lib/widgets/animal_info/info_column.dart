60acafb535fb7f3fd24baceb79352284e9d725f2
renad1 <renadxx17@gmail.com>
2023-06-18 14:57:07 +0300

    add delete Dialog in info page

--- paws_frontend/lib/widgets/animal_info/info_column.dart
+++ paws_frontend/lib/widgets/animal_info/info_column.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 class InfoColumn extends StatelessWidget {
-  const InfoColumn({super.key, required this.info1, required this.info2, required this.info3, required this.info4});
+  const InfoColumn({super.key, required this.info1, required this.info2, required this.info3, required this.info4, required this.info5});
   final String info1;
   final String info2;
   final String info3;
   final String info4;
+  final String info5;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,6 +16,15 @@ class InfoColumn extends StatelessWidget {
         Text(
           info1,
           style: const TextStyle(
+              
+              fontWeight: FontWeight.bold,fontSize: 18),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        Text(
+          info2,
+          style: const TextStyle(
               color: Color.fromARGB(255, 95, 94, 94),
               fontWeight: FontWeight.bold),
         ),
@@ -22,7 +32,7 @@ class InfoColumn extends StatelessWidget {
           height: 12,
         ),
         Text(
-          info2,
+          info4,
           style: const TextStyle(
               color: Color.fromARGB(255, 95, 94, 94),
               fontWeight: FontWeight.bold),
@@ -40,7 +50,7 @@ class InfoColumn extends StatelessWidget {
           height: 12,
         ),
         Text(
-          info4,
+          info5,
           style: const TextStyle(
               color: Color.fromARGB(255, 95, 94, 94),
               fontWeight: FontWeight.bold),
