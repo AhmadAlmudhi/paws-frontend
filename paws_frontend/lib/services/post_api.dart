5a01e8fc167fa9ffd8df00e4dbd2de140fcd04be
AhmadAlmudhi <a.almudhi1419@gmail.com>
2023-06-21 14:13:06 +0300

    create post api

--- paws_frontend/lib/services/post_api.dart
+++ paws_frontend/lib/services/post_api.dart
@@ -1,8 +1,18 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart';
 
-Future<Response> createPost(Map data) async {
+Future<Response> createPost(Map data, List imagesFiles) async {
+  List imageLinks = [];
+
+  for (File file in imagesFiles) {
+    final bytes = await file.readAsBytes();
+    imageLinks.add(bytes);
+  }
+
+  data.addAll({"images": imageLinks});
+
   Uri uri = Uri.parse("http://0.0.0.0:8080/post/create");
   Response response = await post(uri,
       body: jsonEncode(data),
